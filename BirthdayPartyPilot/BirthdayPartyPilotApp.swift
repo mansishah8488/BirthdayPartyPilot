3f562d6b00561131c604ef8a2e60f7f3089a3b10
Mansi Shah <mansishah8488@gmail.com>
2026-07-26 19:54:20 -0700

    Initial Commit

---
+++ BirthdayPartyPilot/BirthdayPartyPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayPartyPilotApp.swift
+//  BirthdayPartyPilot
+//
+//  Created by Mansi Shah on 7/26/26.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayPartyPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
